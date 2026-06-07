b237017200900bf32c4522f38abda441155fc657
Madara-Uchiha-001 <121989958+Madara-Uchiha-001@users.noreply.github.com>
2026-06-07 21:49:41 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import PHARMOS from './PHARMOS_v2.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode><PHARMOS /></StrictMode>
+)
